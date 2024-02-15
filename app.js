79b12cad87427a61c81e75f73807320a53f15137
riza <yswisfree@gmail.com>
2024-02-15 19:46:38 +0800

    fix: better project structure

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
